a4b81fccfa59963a45bc39a30dd401a45e89b0c7
Jason Lee <lwlee2608@gmail.com>
2023-12-28 13:05:42 +0800

    implements encode_fields

--- src/cmdc/mod.rs
+++ src/cmdc/mod.rs
@@ -39,8 +39,31 @@ impl Codec for CmdcCodec {
         }
     }
 
-    fn encode_field(&self, _field: &Field) -> Result<Vec<u8>, Box<dyn Error>> {
-        todo!()
+    fn encode_field(&self, field: &Field) -> Result<Vec<u8>, Box<dyn Error>> {
+        // If field is null, return empty vec
+        if field.is_null {
+            return Ok(vec![]);
+        }
+        // If field has data, use it
+        if !field.data.is_empty() {
+            return Ok(field.data.to_vec());
+        }
+        // Encode field value
+        let field_value = field.get_value()?.unwrap();
+
+        match field.field_type {
+            FieldType::Struct => Ok(self.encode_containers(field_value.as_struct().unwrap())?),
+            FieldType::String => Ok(self.encode_string(field_value.as_string().unwrap())?),
+            FieldType::Int8 => Ok(self.encode_int8(field_value.as_int8().unwrap())?),
+            FieldType::Int16 => Ok(self.encode_int16(field_value.as_int16().unwrap())?),
+            FieldType::Int32 => Ok(self.encode_int32(field_value.as_int32().unwrap())?),
+            FieldType::Int64 => Ok(self.encode_int64(field_value.as_int64().unwrap())?),
+            FieldType::UInt8 => Ok(self.encode_uint8(field_value.as_uint8().unwrap())?),
+            FieldType::UInt16 => Ok(self.encode_uint16(field_value.as_uint16().unwrap())?),
+            FieldType::UInt32 => Ok(self.encode_uint32(field_value.as_uint32().unwrap())?),
+            FieldType::UInt64 => Ok(self.encode_uint64(field_value.as_uint64().unwrap())?),
+            _ => todo!(),
+        }
     }
 }
 
@@ -71,33 +94,36 @@ mod tests {
         container.fields[4].field_type = FieldType::Int8;
         container.fields[5].field_type = FieldType::Struct;
 
-        // field 0 is uint8 1
+        // field 0 is uint8 1, use pattern match
         match container.fields[0].decode_value() {
             Ok(Some(Value::UInt8(v))) => assert_eq!(*v, 1),
             _ => panic!("Not a UInt8"),
         }
-        // field 1 is int32 -20
-        match container.fields[1].decode_value() {
-            Ok(Some(Value::Int32(v))) => assert_eq!(*v, -20),
-            _ => panic!("Not a Int32"),
-        }
 
-        // field 2 is string 'three'
-        match container.fields[2].decode_value() {
-            Ok(Some(Value::String(v))) => assert_eq!(v, "three"),
-            _ => panic!("Not a String"),
-        }
+        // field 1 is int32 -20, use as_int32()
+        let field1 = container.fields[1].decode_value().unwrap().unwrap();
+        assert_eq!(field1.as_int32().unwrap(), -20);
+
+        // field 2 is string 'three', use decode_value() and then value()
+        container.fields[2].decode_value().unwrap();
+        let field2 = container.fields[2].value().unwrap();
+        assert_eq!(field2.as_string().unwrap(), "three");
+
         // field 3 is uint32 4
-        match container.fields[3].decode_value() {
-            Ok(Some(Value::UInt32(v))) => assert_eq!(*v, 4),
-            _ => panic!("Not a UInt32"),
-        }
+        assert_eq!(
+            container.fields[3].get_value().err().unwrap().to_string(),
+            "Field not decoded yet"
+        );
+        container.fields[3].decode_value().unwrap();
+        let field3 = container.fields[3].get_value().unwrap();
+        assert_eq!(field3.unwrap().as_uint32().unwrap(), 4);
+
         // field 4 is null
         assert_eq!(container.fields[4].decode_value().unwrap().is_none(), true);
+
         // field 5 as struct
         let field5 = container.fields[5].decode_value().unwrap().unwrap();
         let nested_container = field5.as_struct().unwrap();
-
         assert_eq!(nested_container.containers.len(), 1);
         assert_eq!(nested_container.containers[0].fields.len(), 1);
         assert_eq!(nested_container.containers[0].fields[0].data, b"100");
--- src/cmdc/value.rs
+++ src/cmdc/value.rs
@@ -8,6 +8,10 @@ impl CmdcCodec {
         self.decode_containers(data)
     }
 
+    pub fn encode_struct(&self, containers: &Containers) -> Result<Vec<u8>, Box<dyn Error>> {
+        self.encode_containers(containers)
+    }
+
     pub fn decode_string<'a>(&self, data: &'a [u8]) -> Result<&'a str, Box<dyn Error>> {
         if data.is_empty() {
             return Ok("");
@@ -34,9 +38,23 @@ impl CmdcCodec {
         Err("Invalid string value".into())
     }
 
+    pub fn encode_string(&self, s: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut data = Vec::new();
+        data.push(b'(');
+        data.extend_from_slice(&s.len().to_string().into_bytes());
+        data.push(b':');
+        data.extend_from_slice(s.as_bytes());
+        data.push(b')');
+        Ok(data)
+    }
+
     pub fn decode_int8(&self, data: &[u8]) -> Result<i8, Box<dyn Error>> {
         let s = from_utf8(data)?;
         Ok(s.parse::<i8>()?)
+    }
+
+    pub fn encode_int8(&self, v: i8) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
     }
 
     pub fn decode_int16(&self, data: &[u8]) -> Result<i16, Box<dyn Error>> {
@@ -44,9 +62,17 @@ impl CmdcCodec {
         Ok(s.parse::<i16>()?)
     }
 
+    pub fn encode_int16(&self, v: i16) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
+    }
+
     pub fn decode_int32(&self, data: &[u8]) -> Result<i32, Box<dyn Error>> {
         let s = from_utf8(data)?;
         Ok(s.parse::<i32>()?)
+    }
+
+    pub fn encode_int32(&self, v: i32) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
     }
 
     pub fn decode_int64(&self, data: &[u8]) -> Result<i64, Box<dyn Error>> {
@@ -54,9 +80,17 @@ impl CmdcCodec {
         Ok(s.parse::<i64>()?)
     }
 
+    pub fn encode_int64(&self, v: i64) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
+    }
+
     pub fn decode_uint8(&self, data: &[u8]) -> Result<u8, Box<dyn Error>> {
         let s = from_utf8(data)?;
         Ok(s.parse::<u8>()?)
+    }
+
+    pub fn encode_uint8(&self, v: u8) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
     }
 
     pub fn decode_uint16(&self, data: &[u8]) -> Result<u16, Box<dyn Error>> {
@@ -64,14 +98,26 @@ impl CmdcCodec {
         Ok(s.parse::<u16>()?)
     }
 
+    pub fn encode_uint16(&self, v: u16) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
+    }
+
     pub fn decode_uint32(&self, data: &[u8]) -> Result<u32, Box<dyn Error>> {
         let s = from_utf8(data)?;
         Ok(s.parse::<u32>()?)
     }
 
+    pub fn encode_uint32(&self, v: u32) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
+    }
+
     pub fn decode_uint64(&self, data: &[u8]) -> Result<u64, Box<dyn Error>> {
         let s = from_utf8(data)?;
         Ok(s.parse::<u64>()?)
+    }
+
+    pub fn encode_uint64(&self, v: u64) -> Result<Vec<u8>, Box<dyn Error>> {
+        Ok(v.to_string().into_bytes())
     }
 }
 
@@ -81,10 +127,27 @@ mod tests {
     use crate::cmdc::CMDC_CODEC;
 
     #[test]
-    fn test_decode_valid_string() {
+    fn test_encode_decode_struct() {
+        let data = b"<1,2,0,452,5222,2>[100,2,]";
+        let containers = CMDC_CODEC.decode_struct(data).unwrap();
+        assert_eq!(containers.containers.len(), 1);
+        let container = &containers.containers[0];
+        assert_eq!(container.fields.len(), 3);
+        assert_eq!(container.fields[0].data, b"100");
+        assert_eq!(container.fields[1].data, b"2");
+        assert_eq!(container.fields[2].data, b"");
+
+        let encoded = CMDC_CODEC.encode_struct(&containers).unwrap();
+        assert_eq!(encoded, data);
+    }
+
+    #[test]
+    fn test_encode_decode_string() {
         let data = b"(5:three)";
         let s = CMDC_CODEC.decode_string(data).unwrap();
         assert_eq!(s, "three");
+        let encoded = CMDC_CODEC.encode_string(s).unwrap();
+        assert_eq!(encoded, b"(5:three)");
     }
 
     #[test]
@@ -99,5 +162,77 @@ mod tests {
         let data = b"(3:three)";
         let err = CMDC_CODEC.decode_string(data).err().unwrap().to_string();
         assert_eq!(err, "Invalid string length, 3 is too short");
+    }
+
+    #[test]
+    fn test_encode_decode_int8() {
+        let data = b"-125";
+        let v = CMDC_CODEC.decode_int8(data).unwrap();
+        assert_eq!(v, -125);
+        let data = CMDC_CODEC.encode_int8(v).unwrap();
+        assert_eq!(data, b"-125");
+    }
+
+    #[test]
+    fn test_encode_decode_int16() {
+        let data = b"1070";
+        let v = CMDC_CODEC.decode_int16(data).unwrap();
+        assert_eq!(v, 1070);
+        let data = CMDC_CODEC.encode_int16(v).unwrap();
+        assert_eq!(data, b"1070");
+    }
+
+    #[test]
+    fn test_encode_decode_int32() {
+        let data = b"-107";
+        let v = CMDC_CODEC.decode_int32(data).unwrap();
+        assert_eq!(v, -107);
+        let data = CMDC_CODEC.encode_int32(v).unwrap();
+        assert_eq!(data, b"-107");
+    }
+
+    #[test]
+    fn test_encode_decode_int64() {
+        let data = b"81345123666616";
+        let v = CMDC_CODEC.decode_int64(data).unwrap();
+        assert_eq!(v, 81345123666616);
+        let data = CMDC_CODEC.encode_int64(v).unwrap();
+        assert_eq!(data, b"81345123666616");
+    }
+
+    #[test]
+    fn test_encode_decode_uint8() {
+        let data = b"250";
+        let v = CMDC_CODEC.decode_uint8(data).unwrap();
+        assert_eq!(v, 250);
+        let data = CMDC_CODEC.encode_uint8(v).unwrap();
+        assert_eq!(data, b"250");
+    }
+
+    #[test]
+    fn test_encode_decode_uint16() {
+        let data = b"8000";
+        let v = CMDC_CODEC.decode_uint16(data).unwrap();
+        assert_eq!(v, 8000);
+        let data = CMDC_CODEC.encode_uint16(v).unwrap();
+        assert_eq!(data, b"8000");
+    }
+
+    #[test]
+    fn test_encode_decode_uint32() {
+        let data = b"8000000";
+        let v = CMDC_CODEC.decode_uint32(data).unwrap();
+        assert_eq!(v, 8000000);
+        let data = CMDC_CODEC.encode_uint32(v).unwrap();
+        assert_eq!(data, b"8000000");
+    }
+
+    #[test]
+    fn test_encode_decode_uint64() {
+        let data = b"8000000000000000000";
+        let v = CMDC_CODEC.decode_uint64(data).unwrap();
+        assert_eq!(v, 8000000000000000000);
+        let encoded = CMDC_CODEC.encode_uint64(v).unwrap();
+        assert_eq!(encoded, data);
     }
 }
