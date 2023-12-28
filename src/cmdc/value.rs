d359098e814d8f236b3106dde161c6f7f88ab777
Jason Lee <lwlee2608@gmail.com>
2023-12-28 11:28:57 +0800

    error handling for valuie.decode_string

--- src/cmdc/value.rs
+++ src/cmdc/value.rs
@@ -10,12 +10,16 @@ impl CmdcCodec {
         if data[0] != b'(' {
             return Err("Invalid string value".into());
         }
+
         for (idx, &c) in data.iter().enumerate().skip(1) {
             if c == b':' {
                 let temp = &data[1..idx];
                 let len = Self::bytes_to_int(temp)? as usize;
                 if idx + 1 + len > data.len() {
-                    return Err("Invalid string length".into());
+                    return Err(format!("Invalid string length, {} is too long", len).into());
+                }
+                if data[idx + 1 + len] != b')' {
+                    return Err(format!("Invalid string length, {} is too short", len).into());
                 }
                 let str = from_utf8(&data[idx + 1..idx + 1 + len])?;
                 return Ok(str);
@@ -63,5 +67,32 @@ impl CmdcCodec {
     pub fn decode_uint64(&self, data: &[u8]) -> Result<u64, Box<dyn Error>> {
         let s = from_utf8(data)?;
         Ok(s.parse::<u64>()?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+    use crate::cmdc::CMDC_CODEC;
+
+    #[test]
+    fn test_decode_valid_string() {
+        let data = b"(5:three)";
+        let s = CMDC_CODEC.decode_string(data).unwrap();
+        assert_eq!(s, "three");
+    }
+
+    #[test]
+    fn test_decode_invalid_string_1() {
+        let data = b"(15:three)";
+        let err = CMDC_CODEC.decode_string(data).err().unwrap().to_string();
+        assert_eq!(err, "Invalid string length, 15 is too long");
+    }
+
+    #[test]
+    fn test_decode_invalid_string_2() {
+        let data = b"(3:three)";
+        let err = CMDC_CODEC.decode_string(data).err().unwrap().to_string();
+        assert_eq!(err, "Invalid string length, 3 is too short");
     }
 }
