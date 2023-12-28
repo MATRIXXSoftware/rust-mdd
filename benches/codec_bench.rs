130ce1e323608b8e49968961d5a772cb71471fb5
Jason Lee <lwlee2608@gmail.com>
2023-12-28 15:48:09 +0800

    use test::black_box for reliable benchmark result

--- benches/codec_bench.rs
+++ benches/codec_bench.rs
@@ -1,15 +1,15 @@
 #![feature(test)]
 extern crate test;
+use test::black_box;
 use test::Bencher;
 
-use rust_mdd::cmdc::CmdcCodec;
 use rust_mdd::cmdc::CMDC_CODEC;
 use rust_mdd::codec::Codec;
 
 #[bench]
 fn bench_decode(b: &mut Bencher) {
     let data = b"<1,18,0,-6,5222,2>[1,20,<1,2,0,452,5222,2>[100],4]";
-    b.iter(|| CMDC_CODEC.decode(data));
+    b.iter(|| black_box(CMDC_CODEC.decode(data)));
 }
 
 #[bench]
@@ -17,7 +17,7 @@ fn bench_encode(b: &mut Bencher) {
     let containers = CMDC_CODEC
         .decode(b"<1,18,0,-6,5222,2>[1,20,<1,2,0,452,5222,2>[100],4]")
         .unwrap();
-    b.iter(|| CMDC_CODEC.encode(&containers));
+    b.iter(|| black_box(CMDC_CODEC.encode(&containers)));
 }
 
 fn main() {}
